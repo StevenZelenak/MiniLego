cf2b0b8b56bf28fde5be8638b1b3450ae2883c48
Steven Zelenak <stevenmzelenak@gmail.com>
2021-01-21 19:39:33 -0600

    Adding LegoMini

---
+++ LegoMini/LegoCharacter/LegoPersonCreation.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMini.LegoCharacter
+{
+    class LegoPersonCreation
+    {
+        //properties
+        public string Name { get; set; }
+        public string Hat { get; set; }
+        public string Face { get; set; }
+        public string Torso { get; set; }
+        public string Legs { get; set; }
+        public string Feet { get; set; }
+        public string Weapon { get; set; }
+
+        //constructor
+        public LegoPersonCreation(string name, string hat, string face, string torso, string legs, string feet, string weapon)
+        {
+            Name = name;
+            Hat = CharacterHatType(hat);
+            Face = CharacterFaceType(face);
+            Torso = CharacterTorsoType(torso);
+            Legs = CharacterLegType(legs);
+            Feet = CharacterFeetType(feet);
+            Weapon = CharacterWeaponType(weapon);
+
+            Console.WriteLine("This is the character you created:");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"Character's name: {Name}");
+            Console.WriteLine($"Character's hat: {Hat}");
+            Console.WriteLine($"Character's face: {Face}");
+            Console.WriteLine($"Character's torso: {Torso}");
+            Console.WriteLine($"Character's legs: {Legs}");
+            Console.WriteLine($"Character's feet: {Feet}");
+            Console.WriteLine($"Character's weapon: {Weapon}");
+            Console.WriteLine("-----------------------------------\n");
+
+        }
+
+        //Methods
+        public string CharacterHatType(string answer)
+        {
+            switch (answer)
+            {
+                case "1":
+                    return "Cowboy";
+                case "2":
+                    return "Space Helmet";
+                case "3":
+                    return "Captain's Hat";
+                default:
+                    return "Fedora";
+            }
+        }
+
+        public string CharacterFaceType(string answer)
+        {
+            switch (answer)
+            {
+                case "1":
+                    return "Happy Face";
+                case "2":
+                    return "Angry Face";
+                case "3":
+                    return "Sad Face";
+                default:
+                    return "No Face (audible gasp!)";
+            }
+        }
+
+        public string CharacterTorsoType(string answer)
+        {
+            switch (answer)
+            {
+                case "1":
+                    return "Space Suit";
+                case "2":
+                    return "Pirate Vest";
+                case "3":
+                    return "Cowboy Jacket";
+                default:
+                    return "Leafs";
+            }
+        }
+
+        public string CharacterLegType(string answer)
+        {
+            switch (answer)
+            {
+                case "1":
+                    return "Space Legs";
+                case "2":
+                    return "Skinny Jeans";
+                case "3":
+                    return "Cowboy Pants";
+                default:
+                    return "Leafs";
+            }
+        }
+
+        public string CharacterFeetType(string answer)
+        {
+            switch (answer)
+            {
+                case "1":
+                    return "Space Boots";
+                case "2":
+                    return "Pirate Boots";
+                case "3":
+                    return "Cowboy Boots";
+                default:
+                    return "Who do you think I am!?!?! I don't need shoes!";
+            }
+        }
+
+        public string CharacterWeaponType(string answer)
+        {
+            switch (answer)
+            {
+                case "1":
+                    return "Hook Hand";
+                case "2":
+                    return "Laser Gun";
+                case "3":
+                    return "Revolver";
+                default:
+                    return "Drill (This drill is the drill that will pierce the heavans!)";
+            }
+        }
+
+    }
+}
---
+++ LegoMini/Program.cs
@@ -0,0 +1,38 @@
+﻿using LegoMini.LegoCharacter;
+using System;
+
+namespace LegoMini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pleas fill in the following character sheet to creat your character.");
+            Console.WriteLine("---------------------------------------");
+            //variables
+            Console.WriteLine("Enter character's name: ");
+            string characterName = Console.ReadLine();
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("From this point on choose a number 1-3 for your choice");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Choose your character's hat ( 1: Cowboy 2: Space Helmet 3: Captain's Hat )");
+            string characterHatNumChoice = Console.ReadLine();
+            Console.WriteLine("Choose your character's face ( 1: Happy Face 2: Angry Face 3: Sad Face )");
+            string characterHeadNumChoice = Console.ReadLine();
+            Console.WriteLine("Choose your character's torso ( 1: Space Suit 2: Pirate Vest 3: Cowboy Jacket )");
+            string characterTorsoNumChoice = Console.ReadLine();
+            Console.WriteLine("Choose your character's legs ( 1: Space Legs 2: Skinny Jeans 3: Cowboy Pants )");
+            string characterLegsNumChoice = Console.ReadLine();
+            Console.WriteLine("Choose your character's feet ( 1: Space Boots 2: Pirate Boots 3: Cowboy Boots )");
+            string characterFeetNumChoice = Console.ReadLine();
+            Console.WriteLine("Choose your character's weapons ( 1: Hook Hand 2: Laser Gun 3: Revolver )");
+            string characterWeaponNumChoice = Console.ReadLine();
+            Console.WriteLine("\n");
+
+            
+
+            //calling character class
+            LegoPersonCreation character1 = new LegoPersonCreation(characterName, characterHatNumChoice, characterHeadNumChoice, characterTorsoNumChoice, characterLegsNumChoice, characterFeetNumChoice, characterWeaponNumChoice);
+        }
+    }
+}
